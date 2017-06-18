1b7b86527bfd851b79f5b3cc23007860a5d8605a
Mark Pors <mark@pors.net>
2017-06-18 17:18:35 +0300

    Support for default start pad added

--- start.sh
+++ start.sh
@@ -54,6 +54,9 @@ DEV_MODE=$(escapeChars ${DEV_MODE:-false})
 MIXPANEL_TOKEN=$(escapeChars ${MIXPANEL_TOKEN:-__mixpanel_token__})
 DB_PARAMETERS=$(escapeChars ${DB_PARAMETERS:-''})
 PROCESS_INBOX=$(escapeChars ${PROCESS_INBOX:-false})
+WELCOME_PAD_SOURCE_ID=$(escapeChars ${WELCOME_PAD_SOURCE_ID:-__welcome_pad_source_id__})
+
+
 cp hackpad/etherpad/etc/etherpad.local.properties.tmpl hackpad/etherpad/etc/etherpad.local.properties
 
 sed -i.bak s/__email_addresses_with_admin_access__/$ADMIN_EMAILS/g hackpad/etherpad/etc/etherpad.local.properties
@@ -98,6 +101,7 @@ sed -i.bak s/__disable_creating_workspaces__/$DISABLE_WORKSPACE_CREATION/g hackp
 sed -i.bak s/__mixpanel_token__/$MIXPANEL_TOKEN/g hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak s/__dbc_parameters__/$DB_PARAMETERS/g hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak s/__process_inbox__/$PROCESS_INBOX/g hackpad/etherpad/etc/etherpad.local.properties
+sed -i.bak s/__welcome_pad_source_id__/$WELCOME_PAD_SOURCE_ID/g hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak "s/^\(etherpad\.imgDefaultDomain = \).*$/\1$CANONICAL_DOMAIN/g" hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak "s/^\(topdomains = \).*$/\1$TOP_DOMAINS/g" hackpad/etherpad/etc/etherpad.local.properties
 sed -i.bak "s/^\(useHttpsUrls = \).*$/\1$USE_HTTPS_URLS/g" hackpad/etherpad/etc/etherpad.local.properties
